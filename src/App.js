9b74d66661dd021ce4a5f5a210ccc9ff1615728b
Charley Chen <zwchenbj@cn.ibm.com>
2020-03-26 22:45:00 +0800

    add axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import User from "./utils/User";
+import avatar1 from "shards-ui/dist/images/common/avatar-1.jpeg";
+import API from "./utils/API";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      name: null,
+      avatar: null,
+      email: null
+    };
+  }
+  render() {
+    const { isLoading, name, avatar, email } = this.state;
+
+    return <User isLoading={isLoading} name={name} avatar={avatar} email={email} />;
+  }
+
+  async componentDidMount() {
+    // Load async data.
+    // Update state with new data.
+    // Re-render our component.
+    let userData = await API.get('/',{
+      params: {
+        results: 1,
+        inc: 'name,email,picture'
+      }
+    });
+     // Parse the results for ease of use.
+     userData = userData.data.results[0];
+
+     // Update state with new data and re-render our component.
+     const name = `${userData.name.first} ${userData.name.last}`;
+     const avatar = userData.picture.large;
+     const email = userData.email;
+
+     this.setState({
+       ...this.state, ...{
+         isLoading: false,
+         name,
+         avatar,
+         email
+       }
+     });
+  }
 }
 
 export default App;
